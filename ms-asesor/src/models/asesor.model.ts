cd9f27f7eb154245d2fa595a1a6e42391686c878
sugatov88 <gusvodiazh@gmail.com>
2021-12-05 15:20:39 -0500

    propiedades cliente y asesor

---
+++ ms-asesor/src/models/asesor.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Asesor extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  documento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+
+  constructor(data?: Partial<Asesor>) {
+    super(data);
+  }
+}
+
+export interface AsesorRelations {
+  // describe navigational properties here
+}
+
+export type AsesorWithRelations = Asesor & AsesorRelations;

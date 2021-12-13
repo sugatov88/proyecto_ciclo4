51f6af63034833028cc8cf0793901e36ba095af2
sugatov88 <gusvodiazh@gmail.com>
2021-12-13 18:35:48 -0500

    vistas de angular

--- frontendAlquiler/src/app/app.module.ts
+++ frontendAlquiler/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { PieDePaginaComponent } from './plantilla/pie-de-pagina/pie-de-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
+    PieDePaginaComponent
   ],
   imports: [
     BrowserModule,

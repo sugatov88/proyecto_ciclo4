c1713c70d0920ef551b5a838314504df5af3e117
AngiePilo99 <90914883+AngiePilo99@users.noreply.github.com>
2021-12-13 21:06:26 -0500

    enrutamiento

--- frontendAlquiler/src/app/app.module.ts
+++ frontendAlquiler/src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { MenuNavbComponent } from './plantilla/menu-navb/menu-navb.component';
-import { FooterComponent } from './plantilla/footer/footer.component';
-=======
 import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
 import { PieDePaginaComponent } from './plantilla/pie-de-pagina/pie-de-pagina.component';
->>>>>>> 51f6af63034833028cc8cf0793901e36ba095af2
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { ErrorComponent } from './plantilla/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    MenuNavbComponent,
-    FooterComponent
-=======
     BarraNavegacionComponent,
-    PieDePaginaComponent
->>>>>>> 51f6af63034833028cc8cf0793901e36ba095af2
+    PieDePaginaComponent,
+    InicioComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
--- frontendAlquiler/src/app/modulos/administracion/administracion-routing.module.ts
+++ frontendAlquiler/src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearAsesorComponent } from './asesor/crear-asesor/crear-asesor.component';
+import { ModificarAsesorComponent } from './asesor/modificar-asesor/modificar-asesor.component';
+import { CrearClienteComponent } from './cliente/crear-cliente/crear-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'crear-asesor',
+    component:CrearAsesorComponent
+  },
+  {
+    path:'modificar-asesor',
+    component: ModificarAsesorComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
